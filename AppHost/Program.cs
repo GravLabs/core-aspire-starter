dd54777f227d52099fe40d0076602c959ee62cc3
Hector Ramos <hramos@graviton-labs.com>
2024-08-05 15:24:19 -0400

    re-org

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,5 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.WebApp_Server>("webapp-server");
+var serverApi = builder.AddProject<Projects.WebApp_Server>("webapp-server")
+    .WithExternalHttpEndpoints();
+
+builder.AddNpmApp("webapp-client", "../webapp.client", "dev")
+    .WithEndpoint(targetPort: 5173, scheme:"https", env: "PORT")
+    .WithReference(serverApi)
+    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+    //.WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
